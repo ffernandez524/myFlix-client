190f12f9ad0bc8e96aba3f6c73d59336f7f4f314
ffernandez524 <flareniel@gmail.com>
2023-05-01 20:48:55 -0400

    adjusting package for netlify deployment

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import { createRoot } from 'react-dom/client';
 import { MainView } from "./components/main-view/main-view"
 import Container from "react-bootstrap/Container";
 
+import "./index.scss";
+
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
   return (
