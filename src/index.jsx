df2754ed360962fc72e602e03b87f470a2a6f18d
ffernandez524 <flareniel@gmail.com>
2023-05-04 20:13:19 -0400

    remove cors

--- src/index.jsx
+++ src/index.jsx
@@ -4,20 +4,6 @@ import Container from "react-bootstrap/Container";
 
 import "./index.scss";
 
-const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://cinenotes.netlify.app'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
-
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
   return (
